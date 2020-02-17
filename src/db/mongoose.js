5851fcea7cdb904a42a472d483d8070a29d7e22d
icepear75@msn.com <ice@0611>
2020-02-16 22:09:54 -0500

    update api

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 //const connectionstring = 'mongodb://f03cc05f-0ee0-4-231-b9ee:xQbityLE9YkOg8T7NivmdC5vCiiyxqa9tOPMICJbxA8AkXZWFUMDdPaDeg5LLyrEutYDHJochNoxekIgaxN7Hw==@f03cc05f-0ee0-4-231-b9ee.documents.azure.com:10255/task-manager-api?ssl=true&replicaSet=globaldb'
 const connectionstring =
-  "mongodb+srv://icepear:ice@0611@cluster0-kj3ov.azure.mongodb.net/test?retryWrites=true&w=majority";
+  "mongodb+srv://icepear:ice@0611@cluster0-kj3ov.azure.mongodb.net/task-manager-api?retryWrites=true&w=majority";
 mongoose.connect(connectionstring, {
   useNewUrlParser: true,
   useCreateIndex: true,
