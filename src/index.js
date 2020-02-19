1e0e8160945bb717508228ac6f2d3b3daa689f0c
icepear75@msn.com <ice@0611>
2020-02-19 00:12:20 -0500

    add cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
-var cors = require("cors");
+//var cors = require("cors");
 require("./db/mongoose");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors({ credentials: true, origin: true }));
-
+//app.use(cors({ origin: "http://localhost:4200" }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 // app.use((req, res, next) => {
 //     if (req.method === 'GET') {
 //         res.send('GET requests are disabled')
